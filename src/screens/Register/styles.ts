c97c2a49c8f56f6b41c14a2d6f7ea747a5bc30d9
joaovitordmoraes <joaovitordmoraes@gmail.com>
2022-06-20 21:56:52 -0300

    Create Register page

---
+++ src/screens/Register/styles.ts
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components/native'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+export const Container = styled.View`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.background};
+    flex: 1;
+  `}
+`
+
+export const Header = styled.View`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.primary};
+    width: 100%;
+    height: ${RFValue(112)}px;
+    align-items: center;
+    justify-content: flex-end;
+    padding-bottom: 18px;
+  `}
+`
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.shape};
+    font-size: ${RFValue(18)}px;
+    font-family: ${theme.fonts.regular};
+  `}
+`
+
+export const Form = styled.View`
+  flex: 1;
+  width: 100%;
+  padding: 24px;
+`
